6a1b4a9a576d4723decc494ecc5f6d1a2432cf17
leandro5050 <89838674+leandro5050@users.noreply.github.com>
2022-07-03 23:15:41 -0300

    Add files via upload

---
+++ blog-posts-list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Leandro 5050</title>
+    <link rel="stylesheet" href="blog.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div class="blog">
+        <?php include 'postListRenderer.php'; ?>
+    </div>
+</body>
+</html>
