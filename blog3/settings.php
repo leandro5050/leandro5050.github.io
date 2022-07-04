53b833ac6d82303fbd0f90b6acd1670439df1424
leandro5050 <89838674+leandro5050@users.noreply.github.com>
2022-07-03 23:31:15 -0300

    Add files via upload

---
+++ blog3/settings.php
@@ -0,0 +1,5 @@
+<?php
+    define('BLOG_POSTS_PATH',  './posts');
+    define('BLOG_RSS_TITLE',  'Analytics Blog | RSS');
+    define('BLOG_RSS_DESCRIPTION',  'Site focado em Horóscopo diário.');
+?>
